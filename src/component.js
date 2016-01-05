586ca7752aede67d2aa7160c2cbab006a3341fb1
ismael-hompanera <ismael.hompanera@anfix.com>
2016-01-05 10:48:13 +0100

    [del] tabIndex

--- src/component.js
+++ src/component.js
@@ -245,7 +245,6 @@ var QuillComponent = React.createClass({
           onDragOver: this.props.ondragOver,
           onDrop: this.props.ondrop,
           onClick: this.props.onClick,
-          tabIndex: 0,
 				}),
         React.DOM.div({
           key: 'link-container ' + Math.random(),
