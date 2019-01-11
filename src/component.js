fcca9a14094e34d26865f77c91252d40994f22d1
Gonzalo Rivas <grivcon@gmail.com>
2019-01-11 12:54:43 +0100

    [ADD] React16.

--- src/component.js
+++ src/component.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var React = require('react'),
+  ReactDOM = require('react-dom'),
   PropTypes = require('prop-types'),
 	createReactClass = require('create-react-class'),
 	QuillToolbar = require('./toolbar'),
